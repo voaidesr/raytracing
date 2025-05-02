b84097e93822fbfa85d51929ac9ccccdd724f43a
Voaides Robert <voaides.r@yahoo.com>
2025-05-02 18:14:46 +0300

    added gamma correction to colors

--- include/color.h
+++ include/color.h
@@ -3,8 +3,11 @@
 
 #include <iostream>
 #include "vec3.h"
+#include "interval.h"
 
 using color = vec3;
+
+double linear_to_gamma(double linear_component);
 
 void write_color(std::ostream& out, const color& pixel_color);
 
--- src/color.cpp
+++ src/color.cpp
@@ -1,13 +1,25 @@
 #include "color.h"
 
+double linear_to_gamma(double linear_component) {
+    if (linear_component > 0)
+        return std::sqrt(linear_component);
+
+    return 0;
+}
+
 void write_color(std::ostream& out, const color& pixel_color) {
     auto r = pixel_color.x();
     auto g = pixel_color.y();
     auto b = pixel_color.z();
 
-    int rbyte = int(255.99 * r);
-    int gbyte = int(255.99 * g);
-    int bbyte = int(255.99 * b);
+    r = linear_to_gamma(r);
+    g = linear_to_gamma(g);
+    b = linear_to_gamma(b);
+
+    static const interval intensity(0.000, 0.999);
+    int rbyte = int(256 * intensity.clamp(r));
+    int gbyte = int(256 * intensity.clamp(g));
+    int bbyte = int(256 * intensity.clamp(b));
 
     out << rbyte << ' ' << gbyte << ' ' << bbyte << '\n';
 }
